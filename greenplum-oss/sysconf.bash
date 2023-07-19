5946e29e0b5db450401a6dc7d5014eac0f5cf071
codermichele <124560019+codermichele@users.noreply.github.com>
2023-07-19 20:10:42 +0800

    Update sysconf.bash

--- greenplum-oss/sysconf.bash
+++ greenplum-oss/sysconf.bash
@@ -43,6 +43,7 @@
   
 pip install conan
 dpkg -i /gpdb_src/greenplum-oss/greenplum-oss-debs/multiarch-support_2.27-3ubuntu1.6_amd64.deb
+dpkg -i /gpdb_src/greenplum-oss/greenplum-oss-debs/perl-modules-5.26_5.26.1-6ubuntu0.7_all.deb
 dpkg -i  /gpdb_src/greenplum-oss/greenplum-oss-debs/*.deb
 
 
